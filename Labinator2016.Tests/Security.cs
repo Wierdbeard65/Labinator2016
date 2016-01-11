dfddd39c8c357a9329df465f82210e9bc8805391
Wierdbeard65 <paul@maggiandpaul.com>
2016-01-10 21:11:14 -0500

    Initial version

---
+++ Labinator2016.Tests/Security.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Labinator2016.Lib.Utilities;
+using NUnit.Framework;
+
+namespace Labinator2016.Tests
+{
+    [TestFixture]
+    public class Security
+    {
+        [Test]
+        public void PasswordHashTest()
+        {
+            string hashed = PasswordHash.CreateHash("Test");
+            Assert.AreEqual(true, PasswordHash.ValidatePassword("Test",hashed));
+            Assert.AreEqual(false, PasswordHash.ValidatePassword("NoTest", hashed));
+        }
+    }
+}
