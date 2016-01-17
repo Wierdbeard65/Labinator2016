40ca1af79603c3627f51cb87698b89f9336e3ebf
Wierdbeard65 <paul@maggiandpaul.com>
2016-01-17 12:28:39 -0500

    bootstrap styling added. _Layout updated for bootstrap.

--- Labinator2016/App_Start/BundleConfig.cs
+++ Labinator2016/App_Start/BundleConfig.cs
@@ -26,22 +26,17 @@ namespace Labinator2016
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        ////"~/Scripts/jquery-{version}.js",
-                        ////"~/Scripts/jquery.dataTables.js",
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.dataTables.js",
                         "~/Scripts/site.js"));
-
             ////bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
             ////            "~/Scripts/jquery.validate*"));
-
             ////bundles.Add(new ScriptBundle("~/bundles/jquery-ui").Include(
-            ////            "~/Scripts/jquery-ui.js",
-            ////            "~/Scripts/jquery.dataTables.js"));
-
+            ////            "~/Scripts/jquery-ui.js"));
             //// Use the development version of Modernizr to develop with and learn from. Then, when you're
             //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             ////bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
             ////            "~/Scripts/modernizr-*"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/jquery.dataTables.min.css",
                       "~/Content/site.css"));
