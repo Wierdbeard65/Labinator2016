111829231211e6d9c702ae9344737349841f32c0
wierdbeard65 <wierdbeard65@gmail.com>
2016-04-02 23:43:36 -0400

    More checking DoxyGen

--- Labinator2016.Lib/Models/Classroom.cs
+++ Labinator2016.Lib/Models/Classroom.cs
@@ -16,32 +16,32 @@ namespace Labinator2016.Lib.Models
     /// <summary>
     /// The Classroom object represents a record in the Classroom table in the database.
     /// </summary>
-    /// A Classroom is an instance of a specific course, which has Seats (student environments)
-    /// attached to it. It also hase a Data Center in which it is running and a Time and Date to start.
+    /// A Classroom is an instance of a specific <see cref="Course"/>, which has a series of <see cref="Seat"/> objectss (student environments)
+    /// attached to it. It also has a <see cref="DataCenter"/> in which it is running and a Time and Date to start.
     public class Classroom
     {
         /// <summary>
-        /// Gets or sets the Classroom identifer which uniquely identifies a specific Classroom
+        /// Gets or sets the Classroom identifier which uniquely identifies a specific Classroom
         /// </summary>
         public int ClassroomId { get; set; }
 
         /// <summary>
-        /// Gets or sets the Indetifier of the Course this Classroom is teaching
+        /// Gets or sets the indentifier of the <see cref="Course"/> this Classroom is teaching
         /// </summary>
         public int CourseId { get; set; }
 
         /// <summary>
-        /// Gets or sets the Identifier of the Data Center this Classroom is housed in
+        /// Gets or sets the Identifier of the <see cref="DataCenter"/> this Classroom is housed in
         /// </summary>
         public int DataCenterId { get; set; }
 
         /// <summary>
-        /// Gets or sets the Intentifier of the User who is the primary Instructor for this Classroom
+        /// Gets or sets the indentifier of the <see cref="User"/> who is the primary Instructor for this Classroom
         /// </summary>
         public int UserId { get; set; }
 
         /// <summary>
-        /// Gets or sets the Identifier for the Sky Tap Project corresponding to the Classroom
+        /// Gets or sets the Identifier for the Sky Tap <see cref="Project"/> corresponding to the Classroom
         /// </summary>
         public string Project { get; set; }
 
@@ -51,17 +51,17 @@ namespace Labinator2016.Lib.Models
         public DateTime Start { get; set; }
 
         /// <summary>
-        /// Gets or sets a representation (not actually held in the Database record) of the Course
+        /// Gets or sets a representation (not actually held in the Database record) of the <see cref="Course"/>
         /// </summary>
         public virtual Course Course { get; set; }
 
         /// <summary>
-        /// Gets or sets a representation (not actually held in the Database record) of the DataCenter
+        /// Gets or sets a representation (not actually held in the Database record) of the <see cref="DataCenter"/>
         /// </summary>
         public virtual DataCenter DataCenter { get; set; }
 
         /// <summary>
-        /// Gets a Javascript-readable string representation of the Start date for diplay.
+        /// Gets a Javascript-readable string representation of the Start date for display.
         /// </summary>
         [NotMapped]
         public string JSDate
